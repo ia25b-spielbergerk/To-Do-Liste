c7c5b7c4874f7d113bd480f4dd272091a4c0a3cd
keanu <spielbergerk@bzz.ch>
2025-10-22 12:32:52 +0200

    Test gemacht die gehen aber es ist nicht so wie es sein sollte weil ich nicht den Input teste

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -1,47 +1,59 @@
 package org.example;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
 
-    @Test
-    void main() {
-
-    }
+    private ArrayList<String> aufgaben;
 
     @BeforeEach
     void setUp() {
-    }
-
-    @AfterEach
-    void tearDown() {
+        aufgaben = new ArrayList<>();
     }
 
     @Test
-    void testMain() {
-    }
-
-    @Test
-    void zeigeMenu() {
+    void testListeIstLeer() {
+        assertEquals(0, aufgaben.size());
     }
 
     @Test
     void aufgabeHinzufuegen() {
+        Main.aufgabeHinzufuegen();
+        aufgaben.add("Test");
+        assertEquals(1, aufgaben.size());
+        aufgaben.add("Hallo");
+        assertEquals(2, aufgaben.size());
     }
 
     @Test
     void alleAufgabenAnzeigen() {
+        aufgaben.add("Aufgabe");
+        assertEquals(1, aufgaben.size());
+        aufgaben.add("Test");
+        assertEquals(2, aufgaben.size());
     }
 
     @Test
     void aufgabeBearbeiten() {
+        aufgaben.add("Alt");
+        aufgaben.set(0, "Neu");
+        assertEquals("Neu", aufgaben.get(0));
     }
 
     @Test
     void aufgabeLoeschen() {
+        aufgaben.add("Löschen");
+        aufgaben.remove(0);
+        assertEquals(0, aufgaben.size());
+
+        aufgaben.add("Test");
+        aufgaben.add("Hallo");
+        aufgaben.remove(0);
+        assertEquals(1, aufgaben.size());
     }
 }
