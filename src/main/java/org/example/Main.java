0641a11a010c91b2d4d5152e1cee4f10dc2f8b83
keanu <spielbergerk@bzz.ch>
2025-10-22 10:08:39 +0200

    zeigeMenu, aufgaben- anzeigen und hinzufügen gemacht

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,83 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
+
+    private static ArrayList<String> aufgaben = new ArrayList<>();
+    private static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
+
+        System.out.println("Willkommen bei deiner To-Do-Liste");
+
+        boolean programmLaueft = true;
+
+        while (programmLaueft) {
+            zeigeMenu();
+            int auswahl = scanner.nextInt();
+            scanner.nextLine();
+
+            if (auswahl == 1) {
+                aufgabeHinzufuegen();
+            } else if (auswahl == 2) {
+                alleAufgabenAnzeigen();
+            } else if (auswahl == 3) {
+                aufgabeBearbeiten();
+            } else if (auswahl == 4) {
+                aufgabeLoeschen();
+            } else if (auswahl == 5) {
+                programmLaueft = false;
+                System.out.println("Programm wird beendet");
+            } else {
+                System.out.println("Ungültige Auswahl");
+            }
+
+            System.out.println();
+        }
+
+    }
+
+    private static void zeigeMenu() {
+        System.out.println("Was willst du momentan machen?");
+        System.out.println("1: Aufgabe hinzufügen");
+        System.out.println("2: Alle Aufgaben anzeigen");
+        System.out.println("3: Aufgabe bearbeiten");
+        System.out.println("4: Aufgabe löschen");
+        System.out.println("5: Programm beenden");
+        System.out.print("Deine Wahl: ");
+    }
+
+    private static void aufgabeHinzufuegen() {
+        System.out.print("Gib die neue Aufgabe ein: ");
+        String neueAufgabe = scanner.nextLine();
+        aufgaben.add(neueAufgabe);
+        System.out.println("Aufgabe wurde hinzugefügt!");
+    }
+
+    private static void alleAufgabenAnzeigen() {
+        if (aufgaben.isEmpty()) {
+            System.out.println("Keine Aufgaben vorhanden.");
+        } else {
+            System.out.println("\n=== Deine Aufgaben ===");
+            for (int i = 0; i < aufgaben.size(); i++) {
+                System.out.println((i + 1) + ". " + aufgaben.get(i));
+            }
+        }
+    }
+
+    private static void aufgabeBearbeiten() {
+        alleAufgabenAnzeigen();
+
+        if (!aufgaben.isEmpty()) {
+            System.out.println("Welche Task willst du bearbeiten: ");
+            int nummer = scanner.nextInt();
+            scanner.nextLine();
+        }
+    }
+
+    private static void aufgabeLoeschen() {
 
     }
 }
